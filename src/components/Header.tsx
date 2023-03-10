698c1f34d94ff7c118ed1af6eec1bbb885ebbdbe
Dmytro Hirich <diman12345677@gmail.com>
2023-03-10 18:39:09 +0200

    set localstage \\ favorites page \\ favorites styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,9 @@ import { ChangeEvent, FormEvent, FC, useCallback } from "react";
 import debounce from "lodash.debounce";
 import { useActions } from "@/hooks/actions";
 import Link from "next/link";
+import { HeaderProps } from "@/modules/interfaces";
 
-const Header: FC = () => {
+const Header: FC<HeaderProps> = ({ setIsfavoritesOpen }) => {
   const { setMovieQuery } = useActions();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -15,17 +16,23 @@ const Header: FC = () => {
     e.preventDefault();
   };
 
+  const openFavHandler = () => {
+    setIsfavoritesOpen(true);
+  };
+
   const debouncedChangeHandler = useCallback(debounce(handleChange, 1000), []);
   const divForSVG = `flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none`;
   const input = `block p-4 pl-10 w-full text-sm text-gray-900 rounded-lg border border-gray-300 bg-white border-[#DADDE1]`;
 
   return (
     <header className={"flex w-full h-fit mt-4 items-center relative"}>
-      <Link href={"/favorites"}>
-        <button className="absolute top-0 right-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition ease-in-out duration-200">
-          FAVORITES
-        </button>
-      </Link>
+      <button
+        className="absolute top-0 right-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition ease-in-out duration-200"
+        onClick={openFavHandler}
+      >
+        FAVORITES
+      </button>
+
       <form className={"h-fit w-[40%] mx-auto"} onSubmit={onSubmit}>
         <div className="relative">
           <div className={divForSVG}>
